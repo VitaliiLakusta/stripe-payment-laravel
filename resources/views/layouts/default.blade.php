d0f09ca55a494b5f373a7e81e49a970f81287880
Vitalii Lakusta <lakusta96@gmail.com>
2015-04-07 19:01:10 +0300

    set out Stripe.js in default.blade.php

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+    <head>
+        <title>Your first Web project</title>
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+
+
+
+    <script src="https://js.stripe.com/v2/"></script>
+    </body>
+
+</html>
